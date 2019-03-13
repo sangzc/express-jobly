ad91e745b386e8c6b953339b9e147877516dede4
Sandy <sangzicao@gmail.com>
2019-03-13 10:44:06 -0700

    add unit test for partialUpdate

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,11 @@
+const partialUpdate = require('../../helpers/partialUpdate');
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
-
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
+      const expectedResult = {query:"UPDATE users SET first_name=$1 WHERE id=$2 RETURNING *", values: ["sandy",9999]}
+      const testResult = partialUpdate("users", {"first_name": "sandy"}, "id", 9999);
+      // FIXME: write real tests!
+      expect(testResult).toEqual(expectedResult);
   });
 });
--- db.js
+++ db.js
@@ -4,6 +4,12 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql:///jobly-test";
+} else {
+  DB_URI = "postgresql:///jobly";
+}
+
 const db = new Client({
   connectionString: DB_URI
 });
