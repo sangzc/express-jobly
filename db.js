814be9d8b3fe3deeb499e7e68b6fb01bc94a68d5
Sandy <sangzicao@gmail.com>
2019-03-12 19:54:40 -0700

    initial commit

---
+++ db.js
@@ -0,0 +1,14 @@
+/** Database setup for jobly. */
+
+
+const { Client } = require("pg");
+const { DB_URI } = require("./config");
+
+const db = new Client({
+  connectionString: DB_URI
+});
+
+db.connect();
+
+
+module.exports = db;
