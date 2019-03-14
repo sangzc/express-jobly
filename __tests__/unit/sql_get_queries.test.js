b68aa738b2446440cf335612352d5cc80f432b0f
Sandy <sangzicao@gmail.com>
2019-03-14 11:38:25 -0700

    make one test for sqlGetQueries function

--- __tests__/unit/sql_get_queries.test.js
+++ __tests__/unit/sql_get_queries.test.js
@@ -13,13 +13,11 @@ num_employees < $3
 
 describe(`test sqlGetQueries() generates a proper query, 
 depending on the number and types of arguments given`, () => {
-  it('search only',
+  it('testing if all 3 arguments are given, that it returns the complete query',
       function () {
-      const expectedResult = `
-                                SELECT handle, name 
-                                FROM companies 
-                                WHERE name ILIKE $1
-                            `
+      const query = "SELECT handle, name FROM companies WHERE name ILIKE $1 AND num_employees >  $2 AND  num_employees < $3"
+      query.replace(/\s+/g, ' ').trim();
+      const expectedResult = {"query": query, "values": ["%g%", 100, 5000]}
       const testResult = sqlGetQueries({"search":"g", "min_employees":100, "max_employees": 5000});
       expect(testResult).toEqual(expectedResult);
   });
