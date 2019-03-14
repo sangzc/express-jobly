c7b8601c701e3d207938b53e168786ef93dfb379
Sandy <sangzicao@gmail.com>
2019-03-14 11:50:35 -0700

    test functionality of sqlGetQueries complete

--- __tests__/unit/sql_get_queries.test.js
+++ __tests__/unit/sql_get_queries.test.js
@@ -53,7 +53,7 @@ describe(`test sqlGetQueries() generates a proper query,
 depending on the number and types of arguments given`, () => {
   it('testing if only search is given, that it returns the correct query',
       function () {
-      const query = "SELECT handle, name FROM companies WHERE name ILIKE $1 "
+      const query = "SELECT handle, name FROM companies WHERE name ILIKE $1"
       query.replace(/\s+/g, ' ').trim();
       const expectedResult = {"query": query, "values": ["%g%"]}
       const testResult = sqlGetQueries({"search":"g", "min_employees":undefined, "max_employees": undefined});
@@ -65,7 +65,7 @@ describe(`test sqlGetQueries() generates a proper query,
 depending on the number and types of arguments given`, () => {
   it('testing if min and max queries are given, that it returns the correct query',
       function () {
-      const query = "SELECT handle, name FROM companies WHERE num_employees >  $2 AND  num_employees < $3"
+      const query = "SELECT handle, name FROM companies WHERE num_employees >  $1 AND  num_employees < $2"
       query.replace(/\s+/g, ' ').trim();
       const expectedResult = {"query": query, "values": [100, 5000]}
       const testResult = sqlGetQueries({"search":undefined, "min_employees":100, "max_employees": 5000});
