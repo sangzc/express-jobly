15fbd391ae989a893e5a12b034d74bac87edbcee
Sandy <sangzicao@gmail.com>
2019-03-13 19:11:13 -0700

    add delete and getByHandle methods on the company model and routes, jsonschema add

--- models/company.js
+++ models/company.js
@@ -28,28 +28,25 @@ class Company {
    * This will return JSON of {company: companyData}
    */
   //TODO:
-  static async getByHandle({handle, name, num_employees, description, logo_url}) {
+  static async getByHandle(handle) {
     
     // insert into database
     const res = await db.query(`
-      INSERT INTO companies (handle, name, num_employees, description, logo_url)
-        VALUES ($1, $2, $3, $4, $5)
-        RETURNING handle, name, num_employees, description, logo_url`, 
-      [handle, name, num_employees, description, logo_url]);
-
+      SELECT handle, name, num_employees, description, logo_url
+      FROM companies
+      WHERE handle = $1`, [handle])
+    
+    if (res.rows.length === 0) {
+      throw { message: `There is no company with handle: ${handle}`, status: 404};
+    }
     // return {companyname, hashedpassword, first_name, last_name, phone}
     return res.rows[0];
   }
 
-  /** This should return the handle and name for all of the company objects. 
-   * It should also allow for the following query string parameters
-   * search: If the query string parameter is passed, a filtered list of handles and names handles should be displayed based on the search term and if the name includes it.
-   * min_employees. If the query string parameter is passed, titles and company handles should be displayed that have a number of employees greater than the value of the query string parameter.
-   * max_employees. If the query string parameter is passed, a list of titles and company handles should be displayed that have a number of employees less than the value of the query string parameter.
-   * If the min_employees parameter is greater than the max_employees paramteter, respond with a 400 status and a message notifying that the parameters are incorrect.
-   * This should return JSON of {companies: [companyData, ...]}
-   */
-  //
+  /** This is a static function to help return all of the companies 
+   *  (There are a lot of conditions in here!)
+  */
+  
   static async getAll({ search, min_employees, max_employees }) {
     // const table = "companies";
     // const columns = ["handle", "name"];
@@ -73,6 +70,11 @@ class Company {
     const { query, values } = sqlForPartialUpdate(table, items, key, id);
     // insert into database
     const res = await db.query(query, values);
+
+    if (res.rows.length === 0) {
+      throw { message: `There is no company with handle: ${handle}`, status: 404};
+    }
+
     return res.rows[0];
   }
 
@@ -80,18 +82,19 @@ class Company {
    * This will return JSON of {message: "Company deleted"}
    */
   //TODO:
-//   static async delete({handle, name, num_employees, description, logo_url}) {
-//     // insert into database
-//     const res = await db.query(`
-//       INSERT INTO companies (handle, name, num_employees, description, logo_url)
-//         VALUES ($1, $2, $3, $4, $5)
-//         RETURNING handle, name, num_employees, description, logo_url`, 
-//       [handle, name, num_employees, description, logo_url]);
+  static async delete(handle) {
+    // delete from database
+    const res = await db.query(`
+       DELETE FROM companies
+       WHERE handle=$1
+       RETURNING handle`, [handle])
 
-//     // return {companyname, hashedpassword, first_name, last_name, phone}
-//     return res.rows[0];
-//   }
-// }
-}
+    if (res.rows.length === 0) {
+      throw { message: `There is no company with handle: ${handle}`, status: 404};
+    }
+    
+  };
+};
+
 
 module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,10 @@ const Company = require("../models/company");
 
 const router = new Router();
 
+const { validate } = require("jsonschema");
+const companySchemaPost = require("../schemas/companySchemaPost");
+const companySchemaPatch = require("../schemas/companySchemaPost");
+
 /**
  * return the handle and name for all of the company objects. 
  * query string parameters, 
@@ -36,8 +40,15 @@ router.get("/", async function (req, res, next){
  * create a new company and return the newly created company.
  * return JSON of {company: companyData} */
 router.post("/", async function (req, res, next){
-
+    
     try{
+        const validation = validate(req.body, companySchemaPost);
+        if (!validation.valid) {
+            return next({
+                status: 400,
+                error: validation.errors.map(e => e.stack);
+            });
+        }
         const newCompanyData = req.body;
         const company = await Company.create(newCompanyData);
         return res.json({company});
@@ -47,10 +58,16 @@ router.post("/", async function (req, res, next){
     }
 })
 
-
+/**
+ * This should return a single company found by its id.
+ * This should return JSON of {company: companyData}
+ */
 router.get("/:handle", async function (req, res, next){
-    try{
-        
+    try {
+        const handle = req.params.handle;
+        const company = await Company.getByHandle(handle);
+
+        return res.json({company});
     } catch(err) {
         return next(err);
     }
@@ -63,10 +80,29 @@ router.get("/:handle", async function (req, res, next){
  */
 router.patch("/:handle", async function (req, res, next) {
     try{
+        const validation = validate(req.body, companySchemaPatch);
+        if (!validation.valid) {
+            return next({
+                status: 400,
+                error: validation.errors.map(e => e.stack);
+            });
+        }
         const handle = req.params;
         const data = req.body;
         const company = await Company.update({...handle, ...data});
         return res.json({company});
+
+    } catch(err) {
+        return next(err);
+    }
+})
+
+router.delete("/:handle", async function (req, res, next) {
+    try{
+
+        const handle = req.params.handle;
+        await Company.delete(handle);
+        return res.json({message: "Company deleted"});
 
     } catch(err) {
         return next(err);
